06989ea373bbee7f56b00f83596413111ceedca0
Zulfa094 <zulfa_94@hotmail.com>
2024-11-26 23:52:41 +0300

    part3

--- controllers/loans.js
+++ controllers/loans.js
@@ -4,50 +4,77 @@ const router = express.Router()
 const Loan = require('../models/loan')
 
 router.get('/', async (req, res) => {
-  const loans = await Loan.find().populate('client')
-  res.render('loan/index.ejs', { loans })
+  try {
+    const loans = await Loan.find({}).populate('client')
+    res.render('loans/index.ejs', { loans })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
 })
 
 router.get('/new', async (req, res) => {
-  res.render('loan/new.ejs')
+  res.render('loans/new.ejs')
 })
 
+// Create a new loan
 router.post('/', async (req, res) => {
-  const loan = await Loan.create({
-    borrower: req.session.user._id,
-    amount: req.body.amount,
-    interestRate: req.body.interestRate,
-    duration: req.body.duration
-  })
-  res.redirect(`/loans/${loan._id}`)
-})
-
-// Show: Loan details
-router.get('/:id', async (req, res) => {
-  const loan = await Loan.findById(req.params.id).populate('borrower')
-  res.render('loan/show.ejs', { loan })
-})
-
-// Edit: Loan update form
-router.get('/:id/edit', async (req, res) => {
-  const loan = await Loan.findById(req.params.id)
-  res.render('loan/edit.ejs', { loan })
-})
-
-router.put('/:id', async (req, res) => {
-  const { amount, interestRate, duration, status } = req.body
-  await Loan.findByIdAndUpdate(req.params.id, {
-    amount,
-    interestRate,
-    duration,
-    status
-  })
-  res.redirect(`/loans/${req.params.id}`)
-})
-
-router.delete('/:id', async (req, res) => {
-  await Loan.findByIdAndDelete(req.params.id)
+  req.body.client = req.session.user._id
+  await Loan.create(req.body)
   res.redirect('/loans')
+})
+
+// Show a specific loan
+router.get('/:loanId', async (req, res) => {
+  try {
+    const loan = await Loan.findById(req.params.loanId).populate('client')
+    res.render('loans/show.ejs', { loan })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+})
+
+// Edit a specific loan (form)
+router.get('/:loanId/edit', async (req, res) => {
+  try {
+    const loan = await Loan.findById(req.params.loanId)
+    res.render('loans/edit.ejs', { loan })
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+})
+
+// Update a specific loan
+router.put('/:loanId', async (req, res) => {
+  try {
+    const loan = await Loan.findById(req.params.loanId)
+    if (loan.client.equals(req.session.user._id)) {
+      await loan.updateOne(req.body)
+      res.redirect('/loans')
+    } else {
+      res.send("You don't have permission to do that")
+    }
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
+})
+
+router.delete('/:loanId', async (req, res) => {
+  try {
+    const loan = await Loan.findById(req.params.loanId)
+    if (loan.client.equals(req.session.user._id)) {
+      await loan.deleteOne()
+      res.redirect('/loans')
+    } else {
+      res.send("You don't have permission to do that")
+    }
+  } catch (err) {
+    console.log(err)
+    res.redirect('/')
+  }
 })
 
 module.exports = router
